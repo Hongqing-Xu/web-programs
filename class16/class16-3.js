4ee2c7a4dfc2a90a7b37b8fbbc4789c7d9fd5d0c
Hongqing-Xu <1980302391@qq.com>
2020-11-16 17:41:18 +0800

    Lesson 17

---
+++ class16/class16-3.js
@@ -0,0 +1,11 @@
+
+
+function orange(){
+    var array =[ 123,45,56,78,89,90,1234,23,88,99]
+    for (let index = 0;index< array.length;index++){
+        const element = array[index];
+        console.log(element); 
+    }
+}
+
+orange();
